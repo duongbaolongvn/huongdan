039dedf8dd077b6cbe4be5501bd474a5b7204553
DBL <blduong.vn@gmail.com>
2021-03-13 22:56:40 +0700

    Initial Commit

---
+++ huongdan/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  huongdan
+//
+//  Created by Duong Bao Long on 3/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
